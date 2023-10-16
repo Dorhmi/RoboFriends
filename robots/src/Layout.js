4aba7a7e9ebec3133e0aeed81e1baef8c638686c
zakaria <zikoxdodo@gmail.com>
2023-10-16 17:54:31 +0100

    fetch from data

--- robots/src/Layout.js
+++ robots/src/Layout.js
@@ -1,6 +1,5 @@
 import React , {Component} from "react";
 import CardsContainer from './Cardscontainer';
-import { robots } from './robots';
 import './Layout.css';
 import Searchbox from './Searchbox';
 
@@ -8,10 +7,17 @@ class Layout extends Component{
     constructor() {
         super()
         this.state = {
-            robots : robots,
+            robots : [],
             searchfield: '',
         }
     }
+
+    componentDidMount () {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots:users}));
+    } 
+
     searchOnChange = (e) => {
         this.setState({searchfield: e.target.value})
     }
@@ -19,17 +25,21 @@ class Layout extends Component{
         const filtredArray = this.state.robots.filter(item => {
             return item.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        return (
-            <header className="header">
-                <div className="header-content">
-                <h1 className="header-title">robofriends</h1>
-                <Searchbox searchchange = {this.searchOnChange} />
-                </div>
-                <div>
-                    <CardsContainer robots={filtredArray} /> 
-                </div>
-            </header>
-        )
+        if(this.state.robots.length === 0) {
+            return <h1 className="load">Loading...</h1>
+        } else {
+            return (
+                <header className="header">
+                    <div className="header-content">
+                    <h1 className="header-title">robofriends</h1>
+                    <Searchbox searchchange = {this.searchOnChange} />
+                    </div>
+                    <div>
+                        <CardsContainer robots={filtredArray} /> 
+                    </div>
+                </header>
+            );
+        }
     }
 }
 
